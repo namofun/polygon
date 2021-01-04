e0cfb042bf3df30c8165ed1b35d301f353d42d93
XiaoYang <670171961@qq.com>
2021-01-04 23:15:42 +0800

    Add default interface.

---
+++ src/DataAccess/PolygonDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Polygon.Entities;
+
+namespace Polygon.Storages
+{
+    /// <summary>
+    /// The marker interface to add polygon <see cref="DbSet{TEntity}"/>s.
+    /// </summary>
+    public interface IPolygonDbContext
+    {
+        DbSet<Executable> Executables { get; set; }
+
+        DbSet<InternalError> InternalErrors { get; set; }
+
+        DbSet<Judgehost> Judgehosts { get; set; }
+
+        DbSet<Judging> Judgings { get; set; }
+
+        DbSet<JudgingRun> JudgingRuns { get; set; }
+
+        DbSet<Language> Languages { get; set; }
+
+        DbSet<Problem> Problems { get; set; }
+
+        DbSet<Rejudging> Rejudgings { get; set; }
+
+        DbSet<Submission> Submissions { get; set; }
+
+        DbSet<SubmissionStatistics> SubmissionStatistics { get; set; }
+
+        DbSet<Testcase> Testcases { get; set; }
+
+        DbSet<Testcase> ProblemAuthors { get; set; }
+    }
+}
--- src/Host/DefaultContext.cs
+++ src/Host/DefaultContext.cs
@@ -1,14 +1,40 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Polygon.Entities;
+using Polygon.Storages;
 using SatelliteSite.IdentityModule.Entities;
 
 namespace SatelliteSite
 {
-    public class DefaultContext : IdentityDbContext<User, Role, int>
+    public class DefaultContext : IdentityDbContext<User, Role, int>, IPolygonDbContext
     {
         public DefaultContext(DbContextOptions options)
             : base(options)
         {
         }
+
+        public virtual DbSet<Executable> Executables { get; set; }
+
+        public virtual DbSet<InternalError> InternalErrors { get; set; }
+
+        public virtual DbSet<Judgehost> Judgehosts { get; set; }
+
+        public virtual DbSet<Judging> Judgings { get; set; }
+
+        public virtual DbSet<JudgingRun> JudgingRuns { get; set; }
+
+        public virtual DbSet<Language> Languages { get; set; }
+
+        public virtual DbSet<Problem> Problems { get; set; }
+
+        public virtual DbSet<Rejudging> Rejudgings { get; set; }
+
+        public virtual DbSet<Submission> Submissions { get; set; }
+
+        public virtual DbSet<SubmissionStatistics> SubmissionStatistics { get; set; }
+
+        public virtual DbSet<Testcase> Testcases { get; set; }
+
+        public virtual DbSet<Testcase> ProblemAuthors { get; set; }
     }
 }
